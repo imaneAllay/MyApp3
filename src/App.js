ddad28f2dda12596714acb9ddcef60f871f65919
Imane Allay <73351438+imaneAllay@users.noreply.github.com>
2023-06-01 22:53:13 +0000

    Test changes codesandbox1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //Imane Allay
 // This is a comment to test if I can push my work from codesandbox
 // An other test : I can't see my updates in github after pushing again
+//test
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
 import { RedStripe } from './RedStripe';
